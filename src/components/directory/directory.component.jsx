f5661fcf59f5c59fd64d8656fe455552fb41e904
AbhinabIO <107983743+AbhinabIO@users.noreply.github.com>
2023-10-24 21:10:42 +0530

    wip

---
+++ src/components/directory/directory.component.jsx
@@ -0,0 +1,14 @@
+import CategoryItem from "../category-item/category-item.component";
+import './directory.styles.scss';
+
+const Directory = ({categories}) => {
+  return (
+    <div className="directories-container">
+      {categories.map((category) => (
+        <CategoryItem key={category.id} category={category} />
+      ))}
+    </div>
+  );
+}; 
+
+export default Directory;
